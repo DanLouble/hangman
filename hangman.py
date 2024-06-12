13cadd9089e97ac8208f78cb507196920e7b18e9
DanLouble <128084601+DanLouble@users.noreply.github.com>
2024-06-12 21:15:05 +0100

    Add files via upload
    My first game for python :) This project was created for the game competition my school is hosting and i was interested in getting better at writing code. enjoy and thanks.

--- hangman.py
+++ hangman.py
@@ -1,4 +1,61 @@
 import random
+import re
+
+# Function checks if the string contains any special character
+def run(newstring):
+
+
+    unwantedCharactersList = re.compile("[@_!#$%^&*('<>?/\|}{~:]")
+    
+    # Pass the string in search 
+    # method of regex object.    
+    if(unwantedCharactersList.search(newstring) == None):
+        return True
+        
+    else:
+        return False
+
+filename = "/usr/share/dict/british-english"
+
+
+#variable that is assigned by grabbing words from the dictionary file 
+DictionaryWords = ([x.strip().lower() for x in open(filename,"r")])
+
+
+#list of words that make it past the filter stage (no special characters)
+validwords = []
+
+invalidwords = []
+
+specialCharactersChecker = []
+
+
+c = 0
+
+#loop to run until valid words has 100 items
+for i in range(len(DictionaryWords)):
+    specialCharactersChecker.append(DictionaryWords[i])
+
+    newstring = (''.join(specialCharactersChecker))
+
+    specialCharactersChecker.clear()
+
+    #checkpoint-works well up to here so far
+    if run(newstring) and 12 > len(newstring) > 4:
+        #adding the grabbed word to the valid word list if it has no special characters
+        validwords.append(newstring)
+        newstring = newstring.replace('',newstring)
+   
+    elif run(newstring) == False:
+        invalidwords.append(newstring)
+        newstring = newstring.replace('',newstring)
+
+        
+#print(validwords)
+
+
+
+#stages of lives left in ascending order
 
 Graphics = [r'''
 ------------           ___    ___   _____     _     _        ____        _    ______    _____
@@ -50,14 +107,16 @@ Graphics = [r'''
 |          
 |          
 |           ''',r'''
+''',r'''
+''',r'''
+''',r'''
 ''']
 
-#list of words 
-wordlist = ['Ability', 'Backing', 'Cabinet', 'Absence', 'Balance', 'Calibre', 'Academy', 'Banking', 'Calling', 'Account', 'Barrier', 'Capable', 'Accused', 'Battery', 'Capital', 'Achieve', 'Bearing', 'Captain', 'Acquire', 'Beating', 'Caption', 'Address', 'Because', 'Capture', 'Advance', 'Bedroom', 'Careful', 'Adverse', 'Believe', 'Carrier', 'Advised', 'Beneath', 'Caution', 'Adviser', 'Benefit', 'Ceiling', 'Against', 'Besides', 'Central', 'Airline', 'Between', 'Centric', 'Airport', 'Billion', 'Century', 'Alcohol', 'Binding', 'Certain', 'Alleged', 'Brother', 'Chamber', 'Already', 'Brought', 'Channel', 'Analyst', 'Burning', 'Chapter', 'Ancient', 'Dealing', 'Charity', 'Another', 'Decided', 'Charlie', 'Anxiety', 'Decline', 'Charter', 'Anxious', 'Default', 'Checked', 'Anybody', 'Defence', 'Chicken', 'Applied', 'Deficit', 'Chronic', 'Arrange', 'Deliver', 'Circuit', 'Arrival', 'Density', 'Classes', 'Article', 'Deposit', 'Classic', 'Assault', 'Desktop', 'Climate', 'Assumed', 'Despite', 'Closing', 'Assured', 'Destroy', 'Closure', 'Attempt', 'Develop', 'Clothes', 'Attract', 'Devoted', 'Collect', 'Auction', 'Diamond', 'College', 'Average', 'Digital', 'Combine', 'Eastern', 'Discuss', 'Comfort', 'Economy', 'Disease', 'Command', 'Edition', 'Display', 'Comment', 'Elderly', 'Dispute', 'Compact', 'Element', 'Distant', 'Company', 'Engaged', 'Diverse', 'Compare', 'Enhance', 'Divided', 'Compete', 'Essence', 'Drawing', 'Complex', 'Evening', 'Driving', 'Concept', 'Evident', 'Dynamic', 'Concern', 'Exactly', 'Factory', 'Concert', 'Examine', 'Faculty', 'Conduct', 'Example', 'Failing', 'Confirm', 'Excited', 'Failure', 'Connect', 'Exclude', 'Fashion', 'Consent', 'Exhibit', 'Feature', 'Consist', 'Expense', 'Federal', 'Contact', 'Explain', 'Feeling', 'Contain', 'Explore', 'Fiction', 'Content', 'Express', 'Fifteen', 'Contest', 'Extreme', 'Filling', 'Context', 'Gallery', 'Finance', 'Control', 'Gateway', 'Finding', 'Convert', 'General', 'Fishing', 'Correct', 'Genetic', 'Fitness', 'Council', 'Genuine', 'Foreign', 'Counsel', 'Gigabit', 'Forever', 'Counter', 'Greater', 'Formula', 'Country', 'Hanging', 'Fortune', 'Crucial', 'Heading', 'Forward', 'Crystal', 'Healthy', 'Founder', 'Culture', 'Hearing', 'Freedom', 'Current', 'Heavily', 'Further', 'Cutting', 'Helpful', 'Illegal', 'Jointly', 'Helping', 'Illness', 'Journal', 'Herself', 'Imagine', 'Journey', 'Highway', 'Imaging', 'Justice', 'Himself', 'Improve', 'Justify', 'History', 'Include', 'Keeping', 'Holding', 'Initial', 'Killing', 'Holiday', 'Inquiry', 'Kingdom', 'Housing', 'Insight', 'Kitchen', 'However', 'Install', 'Knowing', 'Hundred', 'Instant', 'Machine', 'Husband', 'Instead', 'Manager', 'Landing', 'Intense', 'Married', 'Largely', 'Interim', 'Massive', 'Lasting', 'Involve', 'Maximum', 'Leading', 'Natural', 'Meaning', 'Learned', 'Neither', 'Measure', 'Leisure', 'Nervous', 'Medical', 'Liberal', 'Network', 'Meeting', 'Liberty', 'Neutral', 'Mention', 'Library', 'Notable', 'Message', 'License', 'Nothing', 'Million', 'Limited', 'Nowhere', 'Mineral', 'Listing', 'Nuclear', 'Minimal', 'Logical', 'Nursing', 'Minimum', 'Loyalty', 'Pacific', 'Missing', 'Obvious', 'Package', 'Mission', 'Offence', 'Painted', 'Mistake', 'Officer', 'Parking', 'Mixture', 'Ongoing', 'Partial', 'Monitor', 'Opening', 'Partner', 'Monthly', 'Operate', 'Passage', 'Morning', 'Opinion', 'Passing', 'Musical', 'Optical', 'Passion', 'Mystery', 'Organic', 'Passive', 'Portion', 'Outcome', 'Patient', 'Poverty', 'Outdoor', 'Pattern', 'Precise', 'Outlook', 'Payable', 'Predict', 'Outside', 'Payment', 'Premier', 'Overall', 'Penalty', 'Premium', 'Proudly', 'Pending', 'Prepare', 'Project', 'Pension', 'Present', 'Promise', 'Pealing', 'Prevent', 'Promote', 'Perfect', 'Primary', 'Protect', 'Perform', 'Printer', 'Protein', 'Perhaps', 'Privacy', 'Protest', 'Phoenix', 'Private', 'Provide', 'Picking', 'Problem', 'Publish', 'Picture', 'Proceed', 'Purpose', 'Pioneer', 'Process', 'Pushing', 'Plastic', 'Produce', 'Qualify', 'Pointed', 'Product', 'Quality', 'Popular', 'Profile', 'Quarter', 'Section', 'Success', 'Radical', 'Segment', 'Suggest', 'Railway', 'Serious', 'Summary', 'Readily', 'Service', 'Support', 'Reading', 'Serving', 'Suppose', 'Reality', 'Session', 'Supreme', 'Realise', 'Setting', 'Surface', 'Receipt', 'Seventh', 'Surgery', 'Receive', 'Several', 'Surplus', 'Recover', 'Shortly', 'Survive', 'Reflect', 'Showing', 'Suspect', 'Regular', 'Silence', 'Sustain', 'Related', 'Silicon', 'Teacher', 'Release', 'Similar', 'Telecom', 'Remains', 'Sitting', 'Telling', 'Removal', 'Sixteen', 'Tension', 'Removed', 'Skilled', 'Theatre', 'Replace', 'Smoking', 'Therapy', 'Request', 'Society', 'Thereby', 'Require', 'Somehow', 'Thought', 'Reserve', 'Someone', 'Through', 'Resolve', 'Speaker', 'Tonight', 'Respect', 'Special', 'Totally', 'Respond', 'Species', 'Touched', 'Restore', 'Sponsor', 'Towards', 'Retired', 'Station', 'Traffic', 'Revenue', 'Storage', 'Trouble', 'Reverse', 'Strange', 'Turning', 'Rollout', 'Stretch', 'Typical', 'Routine', 'Student', 'Uniform', 'Running', 'Studied', 'Unknown', 'Satisfy', 'Subject', 'Unusual', 'Science', 'Succeed', 'Upgrade', 'Walking', 'Whether', 'Upscale', 'Wanting', 'Willing', 'Utility', 'Warning', 'Winning', 'Variety', 'Warrant', 'Without', 'Various', 'Wearing', 'Witness', 'Vehicle', 'Weather', 'Working', 'Venture', 'Webcast', 'Writing', 'Version', 'Website', 'Written', 'Veteran', 'Wedding', 'Western', 'Victory', 'Weekend', 'Whereas', 'Viewing', 'Welcome', 'Whereby', 'Village', 'Welfare', 'Virtual', 'Violent', 'Visible', 'Waiting']
+
 
 enteredletterlist = []
 
-word = (random.choice(wordlist)).lower()
+word = (random.choice(validwords)).lower()
 
 guesslen = len(word)
 
